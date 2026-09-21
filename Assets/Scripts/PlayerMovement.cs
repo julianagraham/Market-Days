11c23b9fb89f88236b856b80bce43868b900b955
julianagraham <julzlee6@outlook.com>
2026-09-21 13:18:47 -0400

    Add player movement and direction animations + idle animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour 
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed =5f;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator animator;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetBool("IsMoving", movement != Vector2.zero);
+
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat("MoveX", movement.x);
+            animator.SetFloat("MoveY", movement.y);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
     }
 }
